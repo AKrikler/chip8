af43913e8dc1d24b1b871413f151a72ae79d0454
AKrikler <alexander.krikler07@gmail.com>
2026-07-31 12:52:44 -0400

    Implement SDL2 audio with square wave generator

--- main.c
+++ main.c
@@ -8,11 +8,43 @@
 #define SCALE 8
 #define CYCLES_PER_FRAME 200
 
+#define FREQUENCY 440.0
+#define SAMPLE_RATE 44100
+#define VOLUME 12000
+
+void audio_callback(void* userdata, Uint8* stream, int len)
+{
+	Chip8* chip8 = (Chip8*)userdata;
+	Sint16* buffer = (Sint16*)stream;
+	int num_samples = len/sizeof(Sint16);
+
+	static double phase = 0.0;
+	double step = (2 * M_PI * FREQUENCY) / SAMPLE_RATE;
+
+	for (int i = 0; i < num_samples; i++)
+	{
+		if (chip8->sound_timer > 0)
+		{
+			buffer[i] = sin(phase) > 0 ? VOLUME : -VOLUME;
+			phase += step;
+			if (phase > 2 * M_PI) phase -= 2 * M_PI;
+		}
+		else
+		{
+			buffer[i] = 0;
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc < 2) return fprintf(stderr, "Usage: %s <path/to/rom>\n", argv[0]), -1;
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) return fprintf(stderr, "SDL Init Error: %s\n", SDL_GetError()), -1;
+	Chip8 chip8;
+	chip8_init(&chip8);
+	chip8_load_rom(&chip8, argv[1]);
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) return fprintf(stderr, "SDL Init Error: %s\n", SDL_GetError()), -1;
 
 	SDL_Window* window;
 	SDL_Renderer* renderer;
@@ -20,9 +52,18 @@ int main(int argc, char* argv[])
 	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, CHIP8_DISPLAY_WIDTH, CHIP8_DISPLAY_HEIGHT);
 	SDL_RenderSetScale(renderer, SCALE, SCALE);
 
-	Chip8 chip8;
-	chip8_init(&chip8);
-	chip8_load_rom(&chip8, argv[1]);
+	SDL_AudioSpec desired, obtained;
+	memset(&desired, 0, sizeof(desired));
+	desired.freq = SAMPLE_RATE;
+	desired.format = AUDIO_S16SYS;
+	desired.channels = 1;
+	desired.samples = 512;
+	desired.callback = audio_callback;
+	desired.userdata = &chip8;
+	
+	if (SDL_OpenAudio(&desired, &obtained) < 0) fprintf(stderr, "SDL_OpenAudio Error: %s\n", SDL_GetError());
+
+	SDL_PauseAudio(0);
 
 	uint64_t last_time = SDL_GetPerformanceCounter();
 	double target_frametime = 1.0 / 60.0;
@@ -90,6 +131,7 @@ int main(int argc, char* argv[])
 		SDL_Delay(1);
 	}
 
+	SDL_CloseAudio();
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
